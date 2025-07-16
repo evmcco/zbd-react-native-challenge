dee0e24130d94e48d633798d818842c0d78c6447
Evan McCoy <evan.mccoy@gmail.com>
2025-07-16 10:12:15 -0400

    project setup with nativewind

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,12 @@
 import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { Platform, StyleSheet, Text } from 'react-native';
 
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+
+import "../../global.css";
 
 export default function HomeScreen() {
   return (
@@ -16,6 +18,9 @@ export default function HomeScreen() {
           style={styles.reactLogo}
         />
       }>
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome to Nativewind!
+      </Text>
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="title">Welcome!</ThemedText>
         <HelloWave />
