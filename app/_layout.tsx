3ca3857bf0d7287f20fc954ad40e45afd71838c2
Evan McCoy <evan.mccoy@gmail.com>
2025-07-19 13:12:55 -0400

    deleting more unused code

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,21 +2,11 @@ import { AlertsBellIcon } from '@/components/AlertsBellIcon';
 import CustomDrawerContent from '@/components/CustomDrawer';
 import { AlertsProvider } from '@/contexts/AlertsContext';
 import { DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
 import { Drawer } from 'expo-router/drawer';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import 'react-native-reanimated';
 
 export default function RootLayout() {
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
-  }
-
   return (
     <ThemeProvider value={DefaultTheme}>
       <AlertsProvider>
